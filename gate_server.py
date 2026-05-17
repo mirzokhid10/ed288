332dc5bb4ea827a54c8fec3796b271cd27dfcafb
Mirzohid <mxudoyberdiyev21@gmail.com>
2026-05-18 00:33:12 +0500

    Remove secrets and clean history

--- gate_server.py
+++ gate_server.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app)
 
 # ========================================
-# CONFIG — Railway MySQL public credentials
+# CONFIG — Railway MySQL public credentialss
 # ========================================
 MYSQL_CONFIG = {
     'host':     os.getenv("MYSQLHOST",     "localhost"),
